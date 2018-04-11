8d1191ed8552d53af53ef9c01ffc6be4084d59aa
sobakaAxolotl <joelmatosvaz@gmail.com>
2018-04-11 14:42:03 +0100

    Read Coil support added Coment on Parsing part

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -10,28 +10,28 @@ import net.wimpi.modbus.io.*;
 import net.wimpi.modbus.net.*;
 import net.wimpi.modbus.util.*;
 
-
-
 public class Main{
 
+    /*
     static LinearConveyor T1 = new LinearConveyor(0,1,0);
     LinearConveyor T2 = new LinearConveyor(2,3,1);
     LinearConveyor T3 = new LinearConveyor(4,5,2);
-
+    */
 
 
     public static void main(String args[]){
 
-        T1.moveBack();
+        //T1.moveBack();
 
-        Parser.parseXML();
+        //Comentado até haver ficheiro XML
+        //Parser.parseXML();
 
         /* The important instances of the classes mentioned before */
         TCPMasterConnection con = null; //the connection
         ModbusTCPTransaction trans = null; //the transaction
-        WriteCoilRequest req = null; //the request
-        ReadInputDiscretesResponse res = null; //the response
-
+        WriteCoilRequest reqWCoil = null; //the request
+        ReadCoilsRequest reqRCoil = null;
+        ReadCoilsResponse CoilResp = null;
 
         InetAddress addr = null; //the slave's address
         try {
@@ -44,13 +44,10 @@ public class Main{
         System.out.println(addr.getHostName()+ "  "+addr.getHostAddress());
 
         int port = 5503;
+
+        //Not used yet
         int ref = 0; //the reference; offset where to start reading from
         int count = 0; //the number of DI's to read
-
-
-
-
-
 
         con = new TCPMasterConnection(addr);
         con.setPort(port);
@@ -66,35 +63,28 @@ public class Main{
             System.exit(1);
         }
 
-
-
         trans = new ModbusTCPTransaction(con);
 
-
-        int i;
-        for(i=1 ;i < 10; i+=2){
+        while(true){
             try {
-                req = new WriteCoilRequest(i,true);
-                trans.setRequest(req);
+                //Write Coil
+                reqWCoil = new WriteCoilRequest(0,true);
+                trans.setRequest(reqWCoil);
                 trans.execute();
-                System.out.println(i);
+                //Read Coil
+                reqRCoil = new ReadCoilsRequest(1, 1);
+                trans.setRequest(reqRCoil);
+                trans.execute();
+                CoilResp = (ReadCoilsResponse)trans.getResponse();
+                System.out.println("Response : "+CoilResp.getCoils().toString());
             } catch(ModbusException md){
                 System.out.println("Failed to send request");
                 System.exit(1);
             }
+
         }
 
 
     }
 
 }
-
-//Codigo protótipo para enviar sinais Move_Forward e Move_Backward
-/*
-    while(true){
-        try{
-            req = new
-        }
-    }
-
- */
