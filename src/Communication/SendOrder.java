8007ea7dd1d96787ab9d1bb59f8b19a2a413dcc4
sobakaAxolotl <joelmatosvaz@gmail.com>
2018-05-10 23:04:13 +0100

    Send orders new update to select priority transform over unload

--- src/Communication/SendOrder.java
+++ src/Communication/SendOrder.java
@@ -14,16 +14,14 @@ import java.util.Vector;
 
 public class SendOrder extends Modbus implements Runnable {
     private ModbusTCPTransaction trans = null; //the transaction
-    private WriteCoilRequest WCoil1 = null; //the request
-    private WriteCoilRequest WCoil2 = null; //the request
-    private ReadCoilsRequest reqRCoilIdle1 = null;
-    private ReadCoilsRequest reqRCoilIdle2 = null;
+    private WriteCoilRequest WCoil = null; //the request
+    private ReadCoilsRequest reqRCoilIdle = null;
+    private ReadCoilsRequest reqRCoilComplete = null;
 
-    private ReadCoilsResponse CoilRespIdle1 = null;
-    private ReadCoilsResponse CoilRespIdle2 = null;
+    private ReadCoilsResponse CoilRespIdle = null;
+    private ReadCoilsResponse CoilRespComplete = null;
 
-    private int idle1 = 0;
-    private int idle2 = 0;
+    private int idle = 0;
 
 
     private boolean runningOrders;
@@ -44,39 +42,104 @@ public class SendOrder extends Modbus implements Runnable {
 
                 int i = 0; //why?
 
-                if(i == 0){ //for what?
-                    try {
-                        trans = new ModbusTCPTransaction(connect(5503));
-                    } catch (Exception e) {
-                        e.printStackTrace();
-                    }
-                    i = 1; //just try/catch doesn't work? what are you trying to do? maybe you wanted 'while'?
+                try {
+                    trans = new ModbusTCPTransaction(connect(5503));
+                } catch (Exception e) {
+                    e.printStackTrace();
                 }
-                
-                
 
-                while (loop) { //why not just while(true) ??? 
+                while (true) { //why not just while(true) ???
                     //Do stuff :)
                 	Transform t = null;
                 	Unload u = null;
                 	//get first orders of each type
-                	if (!Main.transformReceived.isEmpty()) {
-                		t = Main.transformReceived.get(0);
-                	}
-                	if (!Main.unloadReceived.isEmpty()) {
-                		u = Main.unloadReceived.get(0);
-                	}
-                	//if we have transform
-                	if (t != null) {
-                		//send this order or do what you need
-                		//remove after sending
-                		Main.transformReceived.remove(t);
-                	}
-                	else if (u != null) { //if we don't have any transform order, check if we have unload
-                		//send this order
-                		//remove after sending
-                		Main.unloadReceived.remove(u);
-                	}
+
+                    //Check if idle ie available to receive orders
+                    coilNumber = 0;
+                    reqRCoilIdle = new ReadCoilsRequest(coilNumber, 1);
+                    trans.setRequest(reqRCoilIdle);
+                    try {
+                        trans.execute();
+                    } catch (ModbusException e) {
+                        e.printStackTrace();
+                    }
+                    CoilRespIdle = (ReadCoilsResponse)trans.getResponse();
+                    idle = Integer.parseInt(CoilRespIdle.getCoils().toString().trim());
+                    try {
+                        sleep(30);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+
+                    if(idle == 1) {
+                        if (!Main.transformReceived.isEmpty()) {
+                            t = Main.transformReceived.get(0);
+                        }
+                        if (!Main.unloadReceived.isEmpty()) {
+                            u = Main.unloadReceived.get(0);
+                        }
+                        //if we have transform
+                        if (t != null) {
+                            //send this order or do what you need
+                            //remove after sending
+                            int quantity = Integer.parseInt(t.getQuantity());
+                            System.out.println("Quantity to transform: " + quantity);
+                            if(quantity >= 0){
+                                WCoil = new WriteCoilRequest(1, true);
+                                trans.setRequest(WCoil);
+                                try {
+                                    trans.execute();
+                                } catch (ModbusException e) {
+                                    e.printStackTrace();
+                                }
+
+                                try {
+                                    sleep(30);
+                                } catch (InterruptedException e) {
+                                    e.printStackTrace();
+                                }
+
+
+                                coilNumber = 2;
+                                reqRCoilComplete = new ReadCoilsRequest(coilNumber, 1);
+                                trans.setRequest(reqRCoilComplete);
+                                try {
+                                    trans.execute();
+                                } catch (ModbusException e) {
+                                    e.printStackTrace();
+                                }
+                                CoilRespComplete = (ReadCoilsResponse)trans.getResponse();
+                                int complete = Integer.parseInt(CoilRespComplete.getCoils().toString().trim());
+                                System.out.println("Result of boolean complete part one: " + complete);
+
+                                try {
+                                    sleep(30);
+                                } catch (InterruptedException e) {
+                                    e.printStackTrace();
+                                }
+
+                                if(complete == 1) {
+                                    quantity = quantity - 1;
+                                }
+                            }
+                            if(quantity == 0) {
+                                Main.transformReceived.remove(t);
+                                //Add time stamp;
+                                //Send to data base completed order
+                            }
+
+                            try {
+                                sleep(30);
+                            } catch (InterruptedException e) {
+                                e.printStackTrace();
+                            }
+
+                        } else if (u != null) { //if we don't have any transform order, check if we have unload
+                            //send this order
+                            //remove after sending
+                            Main.unloadReceived.remove(u);
+                        }
+                    }
                 	
                 	/* if you will need orders after sending then it's better to create other vectors 
                 	 * and put them there after sending. You can create them in this class or in Main (probably better), wherever you want
@@ -90,49 +153,7 @@ public class SendOrder extends Modbus implements Runnable {
                 	 * Main.transformSent.add(t);
                 	 * Main.transformReceived.remove(t);
                 	 *  */
-                	
 
-                    //Request State on idle status
-                    coilNumber = 0;
-                    reqRCoilIdle1 = new ReadCoilsRequest(coilNumber, 1);
-                    trans.setRequest(reqRCoilIdle1);
-                    try {
-                        trans.execute();
-                    } catch (ModbusException e) {
-                        e.printStackTrace();
-                    }
-                    CoilRespIdle1 = (ReadCoilsResponse)trans.getResponse();
-                    idle1 = Integer.parseInt(CoilRespIdle1.getCoils().toString().trim());
-
-                    if(i==1) {
-                        System.out.println("Result of boolean Idle1 part one: " + idle1);
-                        i = 2;
-                    }
-
-                    if(idle1 == 1){
-                        WCoil1 = new WriteCoilRequest(1, true);
-                        trans.setRequest(WCoil1);
-                        try {
-                            trans.execute();
-                        } catch (ModbusException e) {
-                            e.printStackTrace();
-                        }
-                        try {
-                            sleep(50);
-                        } catch (InterruptedException e) {
-                            e.printStackTrace();
-                        }
-                        trans.setRequest(reqRCoilIdle1);
-                        try {
-                            trans.execute();
-                        }
-                        catch (ModbusException e) {
-                            e.printStackTrace();
-                        }
-                        CoilRespIdle1 = (ReadCoilsResponse)trans.getResponse();
-                        idle1 = Integer.parseInt(CoilRespIdle1.getCoils().toString().trim());
-                        System.out.println("Result of boolean Idle1 part two: " +idle1);
-                    }
                 }
             }
         };
